9224412ee06560231d4186e661841809262e5f75
Brad <brad@viziworx.com>
2020-01-11 11:45:20 -0500

    removed serviceworker, created ApolloClient, passed in the GraphQL server, and wrapped the app with ApolloProvider

--- todo-react/src/index.js
+++ todo-react/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create ApolloClient and pass in the endpoint of the GraphQL server
+const client = new ApolloClient({
+    uri: "http://localhost:4000/graphql"
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Wrap the app with ApolloProvider which will expose the client throughout the app
+ReactDOM.render(<ApolloProvider client={client}><App /></ApolloProvider>, document.getElementById('root'));
